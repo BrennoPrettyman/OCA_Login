0925acb42cf91d14a30427a197a093dacd747e5a
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-12-04 20:21:34 -0300

    Conexão PHP

--- conexao.php
+++ conexao.php
@@ -1,10 +1,17 @@
 <?php
+$servidor = 'localhost';
+$login = 'root';
+$senha = 'root';
+$banco = 'db_sistema';
+
+$conexao = mysqli_connect($servidor, $login, $senha, $banco);
 
 $nome = $_POST['user'];
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 
+$insert = "INSERT INTO `tb_user` (`id_user`, `nm_user`, `login`, `senha`)
+            VALUES (NULL, '$nome', '$email', '$senha')";
 
-
-echo "Nome: $nome, Email: $email, Senha: $senha";
+$sql = mysqli_query($conexao, $insert);
 ?>
